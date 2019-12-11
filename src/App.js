140256d4fadff0a4e9acb1f73ab20d6cf916983c
Appsius <ilyas.ustunkaya234434@gmail.com>
2019-12-11 15:03:58 +0300

    react app setup, palette conf. done

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import seedColors from './seedColors';
+import Palette from './Palette';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Palette {...seedColors[4]} />
+      </div>
+    );
+  }
+}
+
+export default App;
