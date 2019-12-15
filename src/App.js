9269b753695f38745d464830457dd727c6c19ab7
Appsius <ilyas.ustunkaya234434@gmail.com>
2019-12-15 21:07:03 +0300

    Refactor More Styles

--- src/App.js
+++ src/App.js
@@ -46,9 +46,6 @@ class App extends Component {
           )}
         />
       </Switch>
-      // <div>
-      //   <Palette palette={generatePalette(seedColors[4])} />
-      // </div>
     );
   }
 }
