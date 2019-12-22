cd3674e8d788aeb54adac1f8695310b7596c97ad
Appsius <ilyas.ustunkaya234434@gmail.com>
2019-12-22 20:01:54 +0300

    Finish MiniPalette Delete

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -9,7 +9,7 @@ class PaletteList extends Component {
     this.props.history.push(`/palette/${id}`);
   }
   render() {
-    const { palettes, classes } = this.props;
+    const { palettes, classes, deletePalette } = this.props;
     return (
       <div className={classes.root}>
         <div className={classes.container}>
@@ -19,12 +19,13 @@ class PaletteList extends Component {
           </nav>
           <div className={classes.palettes}>
             {palettes.map(palette => (
-              <Link to={`/palette/${palette.id}`} key={palette.id}>
-                <MiniPalette
-                  {...palette}
-                  handleClick={() => this.goToPalette(palette.id)}
-                />
-              </Link>
+              <MiniPalette
+                {...palette}
+                handleClick={() => this.goToPalette(palette.id)}
+                handleDelete={deletePalette}
+                key={palette.id}
+                id={palette.id}
+              />
             ))}
           </div>
         </div>
